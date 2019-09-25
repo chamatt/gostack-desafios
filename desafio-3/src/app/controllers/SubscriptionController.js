cbdd77aa4cc48b6db368df8c894d5fdeaa78eb7c
chamatt <matt.vicent@gmail.com>
2019-09-25 18:59:20 -0300

    starting subscription

---
+++ desafio-3/src/app/controllers/SubscriptionController.js
@@ -0,0 +1,9 @@
+import to from 'await-to-js';
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class SubscriptionController {
+  async store(req, res) {}
+}
+
+export default new SubscriptionController();
